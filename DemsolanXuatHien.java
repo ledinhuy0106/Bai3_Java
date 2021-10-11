40e89ba93d674e58b34c18d8826ee40abbd8dea5
uyle1234 <91107969+uyle1234@users.noreply.github.com>
2021-10-11 16:07:25 +0700

    Add files via upload

---
+++ DemsolanXuatHien.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class DemsolanXuatHien {
+    public static void main(String[] args) {
+        Scanner sa= new Scanner(System.in);
+        String s="manafa";
+        String cs=sa.nextLine();
+        char[] ch =cs.toCharArray();
+        char[] str=s.toCharArray();
+        int c=0;
+        for(int i=0;i<str.length;i++){
+            if(ch[0]==str[i]){
+                c++;
+            }
+        }
+        System.out.println(c);
+    }
+}
